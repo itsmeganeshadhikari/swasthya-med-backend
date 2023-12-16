6e252beed2bd9e03e8fd4a4e8dd7ab886302ab40
Roman jung <peruhanglimbu@gmail.com>
2023-12-16 11:07:37 +0545

    sus-4 {configure sign up and sign in api }

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,36 @@
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import { errorhandler } from '../utils/error.js';
+import Jwt from 'jsonwebtoken';
+
+export const signUp = async (req, res, next) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10);  //password hashed by using bcryptjs
+    const newUser = new User({ username, email, password:hashedPassword });
+    try {
+        await newUser.save();
+        res
+            .status(201)
+            .json({ message: "user created successfully"});
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const signIn = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorhandler(404, 'user not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorhandler(401, 'wrong credentials'));
+        const token = Jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+        const { password: pass, ...rest } = validUser._doc;  //hiding password information from console
+        res
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import Express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 
+dotenv.config();
 
+mongoose.connect(process.env.MONGO)
+    .then(() => { console.log("connected to mongoDB"); })
+    .catch(() => { console.log("couldn't connect to mongoDB"); });
 
 const app = Express();
+
 app.use(Express.json());
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
     console.log("server listening on port 3000");
